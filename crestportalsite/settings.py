693def7e200b81b80d540eef7008913bce367045
tokelee <abdullahomotoke@gmail.com>
2025-08-27 02:57:02 +0100

    ch: UI update

--- crestportalsite/settings.py
+++ crestportalsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-msm@lqulo8x%dnden5@ingzkhsbt(8+ben+j6d3a)es89_$dqq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
